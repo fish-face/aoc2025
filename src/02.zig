9eaa196fa757ffeade5cf52441259b805f547071
Chris Le Sueur <c.m.lesueur@gmail.com>
2025-12-04 19:39:07 +0000

    Day 02: explain

--- src/02.zig
+++ src/02.zig
@@ -66,6 +66,20 @@ fn step(range: []const u8) Ctxt {
 
     // std.log.debug("range {s}:", .{range});
     // GRIPE for loop can't iterate inclusive ranges
+
+    // Strategy
+    // For each range we break the problem down into the different numbers of digits that occur within the range.
+    // e.g. for 95-115, we cover numbers with 2 and 3 digits.
+    // For each digit number, we have an array (above) of divisors. Any number with that number of digits which is
+    // divisible by that divisor displays a repeated pattern. E.g. 10101 * 12 = 121212.
+    // First though, if the number of digits is even, we need to separately do part1. In this case the test divisor
+    // has the form 101, 1001, 10001, ... etc, e.g. 1001 * 123 = 123123. We add the result of this test to the part1
+    // and part2 counts (how this is calculated is explained below)
+    // Now we test the other divisors. These are prefiltered so e.g. for 6 digits, we don't include 111111, as 1001
+    // divides 111111 so we have already accounted for this in part1.
+    // The final step is that for 6 and 10 there are divisors which cover more than one pattern. These are handled
+    // by subtracting the counts for the lowest common multiples of those divisors. Since there are only two such cases,
+    // they are special cased.
     for (digits(l)..digits(u) + 1) |n_digits| {
         const min = pow(usize, 10, n_digits - 1);
         const max = pow(usize, 10, n_digits);
@@ -73,26 +87,27 @@ fn step(range: []const u8) Ctxt {
         // std.log.debug("  {d}=[{d}, {d}] {any}", .{n_digits, min, max, tds});
         if (n_digits % 2 == 0) {
             const div = pow(usize, 10, n_digits / 2) + 1;
-            const invalid = testRange(div, @max(l, min), @min(u, max));
+            const invalid = sumMultiples(div, @max(l, min), @min(u, max));
             part1 += invalid;
             part2 += invalid;
         }
         for (tds) |div| {
-            part2 += testRange(div, @max(l, min), @min(u, max));
+            part2 += sumMultiples(div, @max(l, min), @min(u, max));
             if (n_digits == 6) {
                 // std.log.debug("  minus", .{});
-                part2 -= testRange(111111, @max(l, min), @min(u, max));
+                part2 -= sumMultiples(111111, @max(l, min), @min(u, max));
             }
             if (n_digits == 10) {
                 // std.log.debug("  minus", .{});
-                part2 -= testRange(1111111111, @max(l, min), @min(u, max));
+                part2 -= sumMultiples(1111111111, @max(l, min), @min(u, max));
             }
         }
     }
     return .{ .part1 = part1, .part2 = part2 };
 }
 
-fn testRange(divisor: usize, l: usize, u: usize) usize {
+/// Sum the numbers divisible by divisor within the inclusive range
+fn sumMultiples(divisor: usize, l: usize, u: usize) usize {
     // advance i to next multiple of divisor if it is not already a multiple
     const first = l + divisor - ((l - 1) % divisor) - 1;
     if (first > u) {
