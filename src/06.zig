977758082f065aab8d95f369c8344263e3129ae4
Chris Le Sueur <c.m.lesueur@gmail.com>
2025-12-06 20:16:41 +0000

    Day 06: all at once baybee

--- src/06.zig
+++ src/06.zig
@@ -11,64 +11,29 @@ pub fn main() !void {
     var p2: usize = 0;
 
     for (0..1) |_| {
-        var lines = try reader.iterLines();
-        var cols = [_][4]u16{[_]u16{undefined} ** 4} ** 1024;
-        var ops = [_]u8{undefined} ** 1024;
-
-        var l: usize = 0;
-        var c: usize = 0;
-
-        while (lines.next()) |line| {
-            c = 0;
-            var entries = std.mem.tokenizeScalar(u8, line, ' ');
-            while (entries.next()) |entry| {
-                if (entry[0] == '+' or entry[0] == '*') {
-                    ops[c] = entry[0];
-                } else {
-                    cols[c][l] = aoc.parse.atoi_stripped(u16, entry);
-                }
-                c += 1;
-            }
-            l += 1;
-        }
-
-        // std.log.debug("{any}", .{ops});
-
-        // NOTE swap to work on arbitrary numbers of lines (e.g. sample)
-        // const num_lines = l - 1;
         const num_lines: usize = 4;
-        const num_cols = c;
-        var acc: u64 = 0;
-
-        // TODO opti can do at the same time as parsing if we keep two accumulators
-        for (cols[0..num_cols], 0..) |col, cc| {
-            const op = ops[cc];
-            acc = if (op == '+') 0 else 1;
-            for (col[0..num_lines]) |num| {
-                if (op == '+') {
-                    acc += num;
-                } else {
-                    acc *= num;
-                }
-            }
-            // std.log.debug("{d}", .{acc});
-            p1 += acc;
-        }
 
         const input = try reader.mmap();
         // TODO opti?
         const line_len = std.mem.findScalar(u8, input, '\n').? + 1;
         // var cols = [_][4]u16{ [_]u16{undefined} ** 4} ** 4096;
 
+        // accumulator for the part2 value of the block
         var block_acc: u64 = undefined;
         var op: u8 = 0;
+        var col_acc = [_]u64{0} ** 4;
         for (0..line_len - 1) |i| {
+            // accumulator for the vertical number
             var num_acc: u64 = 0;
+            // 1 accumulator for each horizontal number
 
             for (0..num_lines) |ll| {
                 const digit = input[i + ll * line_len];
                 // std.log.debug("{d},{d}='{c}'", .{i, ll, digit});
                 if (digit == ' ') continue;
+                col_acc[ll] *= 10;
+                col_acc[ll] += digit - '0';
+                // std.log.debug("{d},{d}='{c}'-->{d}", .{i, ll, digit, col_acc[ll]});
                 num_acc *= 10;
                 num_acc += digit - '0';
             }
@@ -79,9 +44,19 @@ pub fn main() !void {
                 block_acc = num_acc;
                 // std.log.debug("setting op to {c}", .{op});
             } else if (num_acc == 0) {
-                // number in column is zero, and no op: this is a blank column. Accumulate to part2 total.
+                // number in column is zero, and no op: this is a blank column. Accumulate to part totals.
+                if (op == '+') {
+                    p1 += col_acc[0] + col_acc[1] + col_acc[2] + col_acc[3];
+                    // std.log.debug("adding p1 {d} for {c} block {d}",
+                    // .{ col_acc[0] + col_acc[1] + col_acc[2] + col_acc[3], op, i });
+                } else {
+                    p1 += col_acc[0] * col_acc[1] * col_acc[2] * col_acc[3];
+                    // std.log.debug("adding p1 {d} for {c} block {d}",
+                    // .{ col_acc[0] * col_acc[1] * col_acc[2] * col_acc[3], op, i });
+                }
                 p2 += block_acc;
                 op = 0;
+                col_acc = [_]u64{0} ** 4;
                 // std.log.debug("adding {d} for {c} block {d}", .{ block_acc, op, i });
             } else {
                 if (op == '+') {
@@ -91,6 +66,12 @@ pub fn main() !void {
                 }
                 // std.log.debug("applying op {c} to {d}-->{d}", .{op, num_acc, block_acc});
             }
+        }
+
+        if (op == '+') {
+            p1 += col_acc[0] + col_acc[1] + col_acc[2] + col_acc[3];
+        } else {
+            p1 += col_acc[0] * col_acc[1] * col_acc[2] * col_acc[3];
         }
         p2 += block_acc;
     }
