c7981674c7d0c1983506970d816377526334462c
Chris Le Sueur <c.m.lesueur@gmail.com>
2025-12-06 15:23:37 +0000

    Day 04: tracking how accessible stuff is in the grid itself is slower locally but let's see

--- src/04.zig
+++ src/04.zig
@@ -9,19 +9,46 @@ const Pt = aoc.Pt;
 fn accessible(grid: Grid, p: usize) bool {
     // std.log.debug("{d} {any}", .{p, grid.ati(p)});
     if (grid.ati(p)) |v| {
-        if (v != '@') {
+        if (v == '.') {
             return false;
+        } else {
+            return v < 4;
         }
     }
-    var n_neighbours: usize = 0;
-    for (grid.neighbours8(p)) |q| {
-        if (grid.ati(q)) |v| {
-            if (v == '@') {
-                n_neighbours += 1;
+    return false;
+    // var n_neighbours: usize = 0;
+    // for (grid.neighbours8(p)) |q| {
+    //     if (grid.ati(q)) |v| {
+    //         if (v == '@') {
+    //             n_neighbours += 1;
+    //         }
+    //     }
+    // }
+    // return n_neighbours < 4;
+}
+
+// TODO opti: store starting queue of accessible locs to do part2
+fn preprocessGrid(grid: *Grid) usize {
+    var coords = grid.coords();
+    var num_accessible: usize = 0;
+    while (coords.next()) |p| {
+        var n_neighbours: u8 = 0;
+        if (grid.ati(p).? == '.') {
+            continue;
+        }
+        for (grid.neighbours8(p)) |q| {
+            if (grid.ati(q)) |v| {
+                if (v != '.') {
+                    n_neighbours += 1;
+                }
             }
         }
+        grid.seti(p, n_neighbours);
+        if (n_neighbours < 4) num_accessible += 1;
     }
-    return n_neighbours < 4;
+    // std.log.debug("{f}", .{grid});
+
+    return num_accessible;
 }
 
 // fn part1(grid: Grid) usize {
@@ -34,10 +61,10 @@ fn accessible(grid: Grid, p: usize) bool {
 //     }
 //     return n_accessible;
 // }
-//
+
 // I'm sticking with the name
 fn prat2(grid: *Grid) [2]usize {
-    var part1: usize = 0;
+    // var part1: usize = 0;
     var removed: usize = 0;
     var removed_any = true;
     // TODO opti: bitset/vec?
@@ -45,26 +72,30 @@ fn prat2(grid: *Grid) [2]usize {
     var queue = List(usize).initBuffer(&buffer);
     var coords = grid.coords();
     while (coords.next()) |p| {
-        if (accessible(grid.*, p)) {
-            part1 += 1;
-            queue.appendAssumeCapacity(p);
-        }
+        queue.appendAssumeCapacity(p);
+        // if (accessible(grid.*, p)) {
+        //     part1 += 1;
+        //     queue.appendAssumeCapacity(p);
+        // }
     }
 
     while (queue.pop()) |p| {
         if (accessible(grid.*, p)) {
             removed += 1;
             removed_any = true;
-            grid.seti(p, 'x');
+            grid.seti(p, '.');
             for (grid.neighbours8(p)) |q| {
-                if (grid.ati(q) != null) {
+                if (grid.ati(q)) |qv| {
+                    if (qv > 0) {
+                        grid.seti(q, qv - 1);
+                    }
                     queue.appendAssumeCapacity(q);
                 }
             }
         }
     }
 
-    return .{part1, removed};
+    return .{0, removed};
 }
 
 pub fn main() !void {
@@ -80,7 +111,8 @@ pub fn main() !void {
 
     for (0..1) |_| {
         // p1 += part1(grid);
-        const pp1, const pp2 = prat2(&grid);
+        const pp1 = preprocessGrid(&grid);
+        _, const pp2 = prat2(&grid);
         p1 += pp1;
         p2 += pp2;
     }
