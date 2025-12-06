a1d9e6803bede3e5bcec8ee7512225fe3ebb201e
Chris Le Sueur <c.m.lesueur@gmail.com>
2025-12-06 12:01:04 +0000

    Day 04: Do part1 as part of part2 part part part

--- src/04.zig
+++ src/04.zig
@@ -24,19 +24,20 @@ fn accessible(grid: Grid, p: usize) bool {
     return n_neighbours < 4;
 }
 
-fn part1(grid: Grid) usize {
-    var it = grid.coords();
-    var n_accessible: usize = 0;
-
-    while (it.next()) |p| {
-        // UNGRIPE: I worked out how to cast bool to int, the name is just a bit weird
-        n_accessible += @intFromBool(accessible(grid, p));
-    }
-    return n_accessible;
-}
-
+// fn part1(grid: Grid) usize {
+//     var it = grid.coords();
+//     var n_accessible: usize = 0;
+//
+//     while (it.next()) |p| {
+//         // UNGRIPE: I worked out how to cast bool to int, the name is just a bit weird
+//         n_accessible += @intFromBool(accessible(grid, p));
+//     }
+//     return n_accessible;
+// }
+//
 // I'm sticking with the name
-fn prat2(grid: *Grid) usize {
+fn prat2(grid: *Grid) [2]usize {
+    var part1: usize = 0;
     var removed: usize = 0;
     var removed_any = true;
     // TODO opti: bitset/vec?
@@ -44,7 +45,10 @@ fn prat2(grid: *Grid) usize {
     var queue = List(usize).initBuffer(&buffer);
     var coords = grid.coords();
     while (coords.next()) |p| {
-        queue.appendAssumeCapacity(p);
+        if (accessible(grid.*, p)) {
+            part1 += 1;
+            queue.appendAssumeCapacity(p);
+        }
     }
 
     while (queue.pop()) |p| {
@@ -60,7 +64,7 @@ fn prat2(grid: *Grid) usize {
         }
     }
 
-    return removed;
+    return .{part1, removed};
 }
 
 pub fn main() !void {
@@ -71,8 +75,7 @@ pub fn main() !void {
     var grid = try reader.readPaddedGrid(1);
     // std.log.debug("{f}", .{grid});
 
-    const p1 = part1(grid);
-    const p2 = prat2(&grid);
+    const p1, const p2 = prat2(&grid);
 
     try aoc.print("{d}\n{d}\n", .{p1, p2});
 }
