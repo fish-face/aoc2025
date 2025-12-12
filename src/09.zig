ccb63636929c305db891f64ee36e9aebfd9f52f8
Chris Le Sueur <c.m.lesueur@gmail.com>
2025-12-12 01:30:30 +0000

    Day 09: With debug logging in case it breaks

--- src/09.zig
+++ src/09.zig
@@ -147,6 +147,9 @@ pub fn main() !void {
             var point = [_]u64{undefined} ** 2;
             // TODO opti: _stripped is not necessary in parseInts today... was it ever?
             _ = aoc.parse.parseInts(u64, &point, line, ',');
+            if (point[0] == 0) {
+                std.log.debug("{s}", .{line});
+            }
 
             const p = Pt(u64){.x = point[0], .y = point[1]};
             points[n_points] = p;
@@ -171,28 +174,26 @@ pub fn main() !void {
             n_points += 1;
         }
 
-        // // 0=N, 1=E, 2=S, 3=W
-        // for (points[1..n_points], 1..) |p, i| {
-        //     var q = p;
-        //     var dir: i8 = undefined;
-        //     var prev_dir: ?i8 = null;
-        //     for (points[i+1..]) |qq| {
-        //         if (qq.y < q.y) {
-        //             dir = 0;  // N
-        //         } else if (qq.x > q.x) {
-        //             dir = 1;  // W
-        //         } else if (qq.y > q.y) {
-        //             dir = 2;  // S
-        //         } else if (qq.x < q.x) {
-        //             dir = 3;
-        //         }
+        // // min_x[i][j] = minimum x value on path from point i to point j, etc
+        // var min_x = [_][POINTS]u64{[POINTS]u64{undefined} ** POINTS} ** POINTS;
+        // var max_x = [_][POINTS]u64{[POINTS]u64{undefined} ** POINTS} ** POINTS;
+        // var min_y = [_][POINTS]u64{[POINTS]u64{undefined} ** POINTS} ** POINTS;
+        // var max_y = [_][POINTS]u64{[POINTS]u64{undefined} ** POINTS} ** POINTS;
+        // for (points[0..n_points-1], 0..) |p, i| {
+        //     @memset(min_x[i], p.x);
+        //     @memset(max_x[i], p.x);
+        //     @memset(min_y[i], p.y);
+        //     @memset(max_y[i], p.y);
+        //     for (points[i+1..], i+1..) |qq, j| {
+        //         min_x[i][j] = @min(p.x, qq.x);
+        //         max_x[i][j] = @min(p.x, qq.x);
+        //         min_y[i][j] = @min(p.y, qq.y);
+        //         max_y[i][j] = @min(p.y, qq.y);
+        //     }
+        // }
         //
-        //         if (prev_dir) |dd| {
-        //             // 1=L, -1=R
-        //             const turn = (dir - dd) % 4;
-        //         }
-        //
-        //         prev_dir = dir;
+        // for (points[0..n_points-1], 0..) |p, i| {
+        //     for (points[i+1..], i+1..) |qq, j| {
         //     }
         // }
 
@@ -216,20 +217,28 @@ pub fn main() !void {
         std.log.debug("{any}", .{x_divs});
         std.mem.sortUnstable([3]u64, x_divs, {}, lessThan3);
         std.mem.sortUnstable(u64, y_divs, {}, std.sort.asc(u64));
-        // var maxRectState = [_]maxRectInfo{.{.h = 0, .hl = 0, .hr = 0, .w = 0, .bottom_left = false, .bottom_right = false}} ** POINTS;
-        var maxRectState = std.mem.zeroes([POINTS]maxRectInfo);
         var y_prev = y_divs[0];
         var x_prev: u64 = 0;
-        // var a: u64 = 0;
         var inside = false;
 
-        // TODO make better
-        // Create boundaries
-        for (0..n_xpoints) |i| {
-            maxRectState[i].r = x_divs[i][0];
-        }
-        // maxRectState[n_xpoints-1].r = x_divs[x_divs.len-1][0] + 0;
 
+        // const GridInfo = struct {
+        //     inside: bool = false,
+        //     bottom: u64,
+        //     right: u64,
+        // };
+        const buf = try allocator.alloc(bool, n_xpoints * n_ypoints);
+        defer allocator.free(buf);
+        @memset(buf, false);
+        var condensed_grid = aoc.Grid(bool){
+            .data = buf,
+            .width = n_xpoints,
+            .height = n_ypoints,
+        };
+        var x_to_div = std.AutoHashMap(u64, usize).init(allocator);
+        var y_to_div = std.AutoHashMap(u64, usize).init(allocator);
+        defer x_to_div.deinit();
+        defer y_to_div.deinit();
         // We now have an ordered list of x and y coordinates, where (e.g.) an x entry indicates that there was a
         // line on that x coordinate somewhere.
         // We walk the resulting grid of crossing points. Only some of these crossing points correspond to points in the
@@ -238,39 +247,28 @@ pub fn main() !void {
         // shape.
         // We do this by detecting when we go across an x coordinate whose corresponding line segment includes the y
         // layer we are on: this represents us crossing from inside to outside, or outside to inside, the shape.
-        for (y_divs[1..]) |y| {
+        try y_to_div.put(y_divs[0], 0);
+        for (y_divs[1..], 1..) |y, j| {
+            try y_to_div.put(y, j);
             for (x_divs[0..], 0..) |x_, i| {
                 const x, const ya, const yb = x_;
 
                 // std.log.debug("? {d}: {d},{d} --> {s}", .{y, ya, yb, if(!(y > ya and y <= yb)) "flip" else "stay"});
+                try x_to_div.put(x, i);
                 if (x_prev <= x) {
                     // otherwise we are transitioning from the final column back to the first column on a new layer
                     if (inside) {
-                        const h = y - y_prev;
-                        const w = x - x_prev;
-                        std.log.debug(
-                            "rect {s}: {d: >2},{d: >2}--{d: >2},{d: >2} {d: >2}|{d: >2}-{d: >2}",
-                            .{if (inside) " in" else "out", x_prev, y_prev, x, y, y, ya, yb});
-                        const top_left = pointset.contains(.{.x = x_prev, .y = y_prev});
-                        const top_right = pointset.contains(.{.x = x, .y = y_prev});
-                        const bottom_left = pointset.contains(.{.x = x_prev, .y = y});
-                        const bottom_right = pointset.contains(.{.x = x, .y = y});
-                        const curState = maxRectState[i];
-                        maxRectState[i] = .{
-                            .h = curState.h + h,
-                            .hl = if (curState.hl == 0 and !top_left) 0 else curState.hl + h,
-                            .hr = if (curState.hr == 0 and !top_right) 0 else curState.hr + h,
-                            .w = w,
-                            .l = x_prev,
-                            .r = x,
-                            .bottom_left = bottom_left,
-                            .bottom_right = bottom_right,
-                        };
-                        // std.log.debug("{any}, {any}", .{top_left, top_right});
+                        if (i == 2 and j == 3) {
+                            std.log.warn("{d}({d}) {d}({d})", .{x, i, y, j});
+                        }
+                        condensed_grid.set(
+                            .{.x = i, .y = j},
+                            true,
+                            // .{.inside = true, .bottom = y, .right = x}
+                        );
+
                     } else {
-                        maxRectState[i].h = 0;
-                        maxRectState[i].hl = 0;
-                        maxRectState[i].hr = 0;
+
                     }
                 }
                 if (y > ya and y <= yb) {
@@ -279,10 +277,67 @@ pub fn main() !void {
                 }
                 x_prev = x;
             }
-            p2 = @max(p2, maxArea(maxRectState[0..n_xpoints], n_xpoints));
             y_prev = y;
         }
 
+        for (points[0..n_points-1], 0..) |p, i| {
+            main: for (points[i+1..n_points]) |q| {
+                const left = if (p.x < q.x) p else q;
+                const right = if (p.x >= q.x) p else q;
+                const top = if (p.y < q.y) p else q;
+                const bottom = if (p.y >= q.y) p else q;
+
+                const xd_left = x_to_div.get(left.x).? + 1;
+                const xd_right = x_to_div.get(right.x).?;
+                const yd_top = y_to_div.get(top.y).? + 1;
+                const yd_bottom = y_to_div.get(bottom.y).?;
+
+                //         left   right
+                //        /      /
+                //        |   |  |  |
+                // top  --+---+--+--+--
+                //        |XXX|XX|  |
+                //        |XXX|XX|  |
+                //      --+---+--+--+--
+                //        |   |XX|  |
+                // bot  --+---+--+--+--
+                //
+                const priv_q = Pt(u64){.x = 2, .y = 3};
+                const priv_p = Pt(u64){.x = 9, .y = 5};
+                for (x_divs[xd_left..], xd_left..) |x_, ii| {
+                    const x = x_[0];
+                    if (priv_p.x == p.x and priv_p.y == p.y and priv_q.x == q.x and priv_q.y == q.y) {
+                        std.log.debug("walking horizontally {d} ({d}) {d}, {d}: {any} {any}", .{
+                            x, ii, yd_top, yd_bottom,
+                            condensed_grid.at(.{.x = ii, .y = yd_top}),
+                            condensed_grid.at(.{.x = ii, .y = yd_bottom}),
+                        });
+                    }
+                    // std.log.debug("{d} {d} {d}x{d}", .{ii, yd_top, condensed_grid.width, condensed_grid.height});
+                    if (yd_top >= condensed_grid.height) continue :main;
+                    if (yd_bottom >= condensed_grid.height) continue :main;
+                    if (!condensed_grid.at(.{.x = ii, .y = yd_top})) continue :main;
+                    if (!condensed_grid.at(.{.x = ii, .y = yd_bottom})) continue :main;
+                    if (x >= right.x) break;
+                }
+
+                for (y_divs[yd_top..], yd_top..) |y, ii| {
+                    if (priv_p.x == p.x and priv_p.y == p.y and priv_q.x == q.x and priv_q.y == q.y) {
+                        std.log.debug("walking vertically {d} ({d}) {d}, {d}: {any} {any}", .{
+                            y, ii, xd_left, xd_right,
+                            condensed_grid.at(.{.y = ii, .x = xd_left}),
+                            condensed_grid.at(.{.y = ii, .x = xd_right}),
+                        });
+                    }
+                    if (y >= bottom.y) break;
+                    if (!condensed_grid.at(.{.y = ii, .x = xd_left})) continue :main;
+                    if (!condensed_grid.at(.{.y = ii, .x = xd_right})) continue :main;
+                }
+
+                // std.log.debug("valid {any}--{any} = {d}", .{p, q, area(p, q)});
+                p2 = @max(p2, area(p, q));
+            }
+        }
 
         p1 += best_area;
         p2 += 0;
